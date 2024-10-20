26b76d7f65c9cb8ba87c1b35731edd71d48fb7f1
Tristan Perrotti <perrotti.tristan@gmail.com>
2024-10-20 04:12:23 -0400

    created components

---
+++ portfolio/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar  = () => {
+  return (  
+    <nav className="navbar">
+      <div>
+        <h1>Tristan Perrotti</h1>
+      </div>
+      <div className="links">
+        <a href="/">About Me</a>
+        <a href="/">Portfolio</a>
+        <a href="/">Contact</a>
+        <a href="/">Resume</a>
+      </div>
+    </nav>
+  );
+}
+ 
+export default Navbar;
